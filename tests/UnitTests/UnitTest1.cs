f51dbe28eabdcdecd52663393bdd777cb09de5a2
Sreelekha <s531382@nwmissouri.edu>
2019-02-08 16:48:19 -0600

    commit

--- tests/UnitTests/UnitTest1.cs
+++ tests/UnitTests/UnitTest1.cs
@@ -5,10 +5,29 @@ namespace UnitTests
 {
     public class UnitTest1
     {
+       
         [Fact]
-        public void Test1()
+        public void CubeofANumber()
         {
+            var expected = 27;
+            var a = 3;
+            
+            var actual = Cube(a);
+            Assert.Equal(expected,actual);
+        }
 
+          [Theory]
+        [InlineData(8,2)]
+        [InlineData(64,4)]
+        [InlineData(125, 5)]
+        public void Cube_WorksWithManyInputs(int exp, int n1)
+        {
+            Assert.Equal(exp, Cube(n1));
+        }
+
+        int Cube(int x)
+        {
+            return x*x*x;
         }
     }
 }
